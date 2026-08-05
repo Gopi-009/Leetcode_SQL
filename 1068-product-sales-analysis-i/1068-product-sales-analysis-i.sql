8fc804d93799227b2c422ec3d7a0d08df816998d
DADI GOPI MANI CHAND <24ng5a6101@usharama.in>
2026-08-05 06:37:55 +0530

    Time: 1341 ms (46.61%), Space: 0B (100%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select p.product_name,s.year,s.price from 
+sales as s inner join product as p on s.product_id = p.product_id
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
