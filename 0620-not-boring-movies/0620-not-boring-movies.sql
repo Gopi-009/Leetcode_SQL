3734f297ecd1182ebf93767bd2cf0881e655e61d
DADI GOPI MANI CHAND <24ng5a6101@usharama.in>
2026-08-05 07:07:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub - Time: O(N log N), Space: O(N)

---
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -0,0 +1,7 @@
+select id, movie, description, rating from Cinema
+where id%2=1 and description not like '%boring%'
+order by id desc;
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
